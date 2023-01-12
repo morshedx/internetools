2657a9be70d8e787e9b7b932d440ae7dfb61a2b5
morshedx <morshedx@users.noreply.github.com>
2023-01-13 03:54:54 +0600

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import '@/styles/global.css';
+
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import type { AppProps } from 'next/app';
+import { useState } from 'react';
+
+const App = ({ Component, pageProps }: AppProps) => {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
+};
+
+export default App;
